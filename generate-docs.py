fa37d69289605e6b7e13206c966f7475a9c94cf3
AlcatrazEscapee <alex@molleroneill.com>
2023-01-08 17:11:13 -0500

    Update with more docs, functions, better formatting and highlighting.

--- generate-docs.py
+++ generate-docs.py
@@ -1,5 +1,7 @@
 import re
 
+TYPES = ('int', 'str', 'function', 'list', 'heap', 'dict', 'set', 'vector', 'any', 'iterable')
+
 def main():
     with open('../cordy/docs/stdlib.md', 'r', encoding='utf-8') as f:
         text = f.read()
@@ -16,6 +18,9 @@ def main():
                 match = re.match(r'###[^`]+`[<>A-Z, ]*([A-Za-z_]+)[`\(]', line)
                 assert match, 'No match: %s' % repr(line)
                 current = match.group(1)
+                line = line[line.index('`'):]  # Skip the leading header
+                assert line.startswith('`') and line.endswith('`')
+                line = line.replace('`', '\\n```\\n')[2:-2]  # Make the top code block a full one, and trim excess newlines
                 buffer = [line]
             else:
                 buffer.append(line)
@@ -38,6 +43,9 @@ export function getHovers(): Map<string, string> {
         f.write("""    ]);
 }
 """)
+
+    print('Functions (for syntaxes/cordy.tmLanguage.json)')
+    print('"\\\\b(' + '|'.join(k for k in docs.keys() if k not in TYPES) + '\\\\b)"')
 
 if __name__ == '__main__':
     main()
